d81f5bcc6ee1429b1b147c5daeaaaafd85f0ee29
Ethan <ethanintoronto@gmail.com>
2025-03-07 20:52:35 -0500

    fixed save path, minor changes to plots

--- scripts/inlaid_vol_area_plotter.py
+++ scripts/inlaid_vol_area_plotter.py
@@ -9,10 +9,13 @@ plt.rcParams['font.sans-serif'] = ['Arial'] # Choose preferred fonts
 
 dir  = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 date = datetime.now().strftime("%Y-%m-%d")
+#date = "2025-03-05"
+
+
 period = "500"
-gamma = "20"
+gamma = "2"
 timestep = "0p001"
-id = "005"
+id = "000"
 base = "_gamma_"+gamma+"_T_"+period+"_timestep_"+timestep+"_"+id
 run = "\\data\\"+date+"\\"+date+base+"\\"
 file = "Single_cell_"+"Area"+base+".txt"
@@ -28,19 +31,17 @@ volume_time = VolumeData[:,0]*10
 volume = VolumeData[:,1] 
 # Create the main figure and axis
 fig, ax = plt.subplots()
-ax.plot(volume_time, volume, label='Volume vs. Time', color='blue')
-ax.set_xlabel(r'Time ($\tau$)')
-ax.set_ylabel('Volume')
-ax.set_title('Cellular Shape Deformation')
-ax.legend()
+ax.plot(volume_time, volume, color='blue')
+ax.set_xlabel(r'Time ($\tau$)', fontsize = 14)
+ax.set_ylabel('Volume', fontsize = 14)
+ax.set_title('Uniform Facial Stiffness', fontsize = 18)
 
 # Create the inset axis
-ax_inset = inset_axes(ax, width="40%", height="40%", loc='upper right')
-ax_inset.plot(area_time, area, label='Area', color='red')
+ax_inset = inset_axes(ax, width="40%", height="30%", loc='upper right')
+ax_inset.plot(area_time, area, color='red')
 ax_inset.tick_params(labelsize=8)
-ax_inset.set_xlabel(r'Time ($\tau$)', fontsize=8)
-ax_inset.set_ylabel('Area', fontsize=8)
-ax_inset.legend(fontsize=6)
+ax_inset.set_xlabel(r'Time ($\tau$)', fontsize=12)
+ax_inset.set_ylabel('Area', fontsize=12)
 plt.savefig("plots\\"+ date+"\\"+date+base+"\\"+date+base+"inlaidVA")
 
 plt.show()
--- scripts/plotData.py
+++ scripts/plotData.py
@@ -9,10 +9,13 @@ plt.rcParams['font.sans-serif'] = ['Arial'] # Choose preferred fonts
 
 dir  = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 date = datetime.now().strftime("%Y-%m-%d")
+date = "2025-03-05"
+
+
 period = "500"
 gamma = "2"
 timestep = "0p001"
-id = "003"
+id = "001"
 base = "_gamma_"+gamma+"_T_"+period+"_timestep_"+timestep+"_"+id
 run = "\\data\\"+date+"\\"+date+base+"\\"
 tests =  ["Area", "Centroid", "Volume", "MaxForce", "GeoCentroid"]
@@ -20,32 +23,38 @@ plot = ["Single_cell_"+x+base+".txt" for x in tests]
 filenames = [dir + run + file for file in plot] 
 
 for index, file in enumerate(filenames):
-    if tests[index] == "Centroid":
-        labels = ["Centroid_x", "Centroid_y","Centroid_z"]
-    elif tests[index] == "GeoCentroid":
-        labels = ["GeoCentroid_x","GeoCentroid_y","GeoCentroid_z"]
-    elif tests[index] == "MaxForce":
-        labels = ["MaxForce", "MaxForce_x","MaxForce_y","MaxForce_z"]
-    else:
-        labels = [tests[index]]
     data = np.loadtxt(file, delimiter = ",")
     time = data[:,0]
+    if tests[index] == "Centroid":
+        plotLabels = [r'Centroid x ($\mu$m)', r'Centroid y ($\mu$m)',r'Centroid z ($\mu$m)']
+        saveLabels = ["Centroid x", "Centroid y","Centroid z"]
+        data[:,1:] = data[:,1:]*7 
+    elif tests[index] == "GeoCentroid":
+        plotLabels = [r'Geometric Centroid x ($\mu$m)',r'Geometric Centroid y ($\mu$m)',r'Geometric Centroid z ($\mu$m)']
+        saveLabels = ["GeoCentroid x", "GeoCentroid y","GeoCentroid z"]
+        data[:,1:] = data[:,1:]*7 
+    elif tests[index] == "MaxForce":
+        plotLabels = ["Max Force", "Max Force x","Max Force y","Max Force z"]
+        saveLabels = ["Max Force", "Max Force x","Max Force y","Max Force z"]
+    else:
+        saveLabels= [tests[index]]
+        plotLabels= [tests[index]]
+
     #Convert time from simulation time to units of tau
     #simulation timestep = 0.01tau 
     #time recorded = time_*timestep = time_*0.001 -> time in minutes = time recorded*100tau*0.5 tau/min
 
     #keeping units of tau for now tau = time*timestep*1000(time/timesteps)*1/100(tau/time):
-    time = time*10
-
+    
+    time = time*10    
     indep = data[:,1:]
     for i in range(len(indep[0])):
         plt.figure()
-        plt.plot(time, indep[:,i], label = labels[i])
-        plt.xlabel("Time", fontsize = "large")
-        plt.ylabel(labels[i], fontsize = "large")
-        plt.title(labels[i])
-        plt.legend()
+        plt.plot(time, indep[:,i])
+        plt.xlabel(r'Time ($\tau$)', fontsize = 14)
+        plt.ylabel(plotLabels[i], fontsize = 14)
+        plt.title(str(saveLabels[i])+" with Stiff Boundary (10 midsteps)", fontsize = 18)
         if not os.path.exists("plots\\"+ date+"\\"+date+base+"\\"):
             os.makedirs("plots\\"+ date+"\\"+date+base)
-        plt.savefig("plots\\"+ date+"\\"+date+base+"\\"+labels[i])
+        plt.savefig("plots\\"+ date+"\\"+date+base+"\\"+date+base+saveLabels[i])
 plt.show()
